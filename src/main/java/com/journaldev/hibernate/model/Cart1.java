76c392b18ac9610f24bc475ad722295945101e86
Rumman Bin Ashraf <rumman@therapservices.net>
2016-10-19 14:13:07 +0600

    Initial Commit

---
+++ src/main/java/com/journaldev/hibernate/model/Cart1.java
@@ -0,0 +1,59 @@
+package com.journaldev.hibernate.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cart")
+public class Cart1 {
+
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "cart_total")
+    private double total;
+
+    @ManyToMany(targetEntity = Item1.class, cascade = { CascadeType.ALL })
+    @JoinTable(name = "Cart_Items",
+            joinColumns = { @JoinColumn(name = "cart_id") },
+            inverseJoinColumns = { @JoinColumn(name = "item_id") })
+    private Set<Item1> items;
+
+//Getter Setter methods
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Set<Item1> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<Item1> items) {
+        this.items = items;
+    }
+}
---
+++ src/main/java/com/journaldev/hibernate/model/Item1.java
@@ -0,0 +1,50 @@
+package com.journaldev.hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Item")
+public class Item1 {
+
+    @Id
+    @Column(name="item_id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="item_price")
+    private double price;
+
+    @Column(name="item_desc")
+    private String description;
+
+// Getter Setter methods
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
